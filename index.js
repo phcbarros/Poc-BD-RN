a7594bf70d136d0ffd3fcece24806e54f1221339
phcbarros <paulo.barros@outlook.com.br>
2021-05-03 09:45:02 -0300

    Criando poc com RealmDB - 4 Corrigir erro ao carregar fonte

--- index.js
+++ index.js
@@ -5,4 +5,6 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+FontAwesome.loadFont()
 AppRegistry.registerComponent(appName, () => App)
